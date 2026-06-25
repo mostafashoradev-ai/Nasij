b4f7295f91e9972d250b073ceaece64fd08f104f
Mostafa Shora <mm7326745@gmail.com>
2026-06-25 22:20:44 +0300

    first commit

--- js/firebase.js
+++ js/firebase.js
@@ -6,13 +6,13 @@ import { getAuth }      from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js';
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey : "AIzaSyDyruu_qm54El5UjIofDcVOXsQ93BFMjhY",
+  authDomain : "nasij-80e90.firebaseapp.com",
+  projectId : "nasij-80e90",
+  storageBucket : "nasij-80e90.firebasestorage.app",
+  messagingSenderId : "659949075952",
+  appId : "1:659949075952:web:e302b85552bb4cb49bf135",
+  measurementId : "G-9GV0XWB0DB"
 };
 
 const app = initializeApp(firebaseConfig);
